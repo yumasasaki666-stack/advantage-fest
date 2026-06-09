275b8dfadc95b24739dae1ffb1f10ffe4f1791d0
yumasasaki666-stack <yumasasaki666@gmail.com>
2026-06-09 17:44:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -101,13 +101,13 @@ document.addEventListener('DOMContentLoaded', () => {
                     "ポケカバトルファクトリー": `
                         <li><strong>形式：</strong>レンタルデッキ対戦</li>
                         <li><strong>所要時間：</strong>約80分</li>
-                        <li><strong>参加費：</strong>イベントチケット2枚</li>
+                        <li><strong>参加費：</strong>イベントチケット2枚（1枚300円）</li>
                         <li><strong>対象：</strong>初心者OK</li>
                     `,
                     "スリーストリークバトル": `
                         <li><strong>形式：</strong>ガンスリンガー</li>
                         <li><strong>所要時間：</strong>25～80分（連勝数で変化）</li>
-                        <li><strong>参加費：</strong>イベントチケット1枚</li>
+                        <li><strong>参加費：</strong>イベントチケット1枚（1枚300円）</li>
                     `,
                     "ゆびをふるポケカ体験": `
                         <li><strong>形式：</strong>カジュアルバトル</li>
